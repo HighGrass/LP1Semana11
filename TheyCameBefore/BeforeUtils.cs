dd73e989ed616c10677e65332fa07d47684d53eb
matv <matv.oficial@gmail.com>
2024-05-24 18:41:49 +0100

    TheyCameBefore - BeforeUtils

---
+++ TheyCameBefore/BeforeUtils.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class BeforeUtils
+{
+    public static IEnumerable<T> GetTheOnesBefore<T>(IEnumerable<T> items, T item) where T : struct, IComparable<T> { return items.Where(i => i.CompareTo(item) < 0); }
+}

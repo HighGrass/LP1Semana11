8e1f975d6e6cf181ae494846659c480f00d5195f
matv <matv.oficial@gmail.com>
2024-05-24 18:33:35 +0100

    Checker fixes

--- HowManyOfThisType/Checker.cs
+++ HowManyOfThisType/Checker.cs
@@ -3,17 +3,10 @@ using System.Collections.Generic;
 
 public static class Checker
 {
-    // This method counts the number of items in the collection that are of type T
     public static int HowManyOfType<T>(IEnumerable<object> items)
     {
         int count = 0;
-        foreach (var item in items)
-        {
-            if (item is T)
-            {
-                count++;
-            }
-        }
+        foreach (var item in items) if (item is T) count++;
         return count;
     }
 }
